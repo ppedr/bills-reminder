ad00122792c643d0b6a9e2095ef8e997dca81a95
Pedro Freitas <49600673+ppedr@users.noreply.github.com>
2025-08-09 17:35:45 -0300

    Use Lombok to reduce boilerplate

--- src/main/java/com/example/bills/controller/BillController.java
+++ src/main/java/com/example/bills/controller/BillController.java
@@ -4,6 +4,7 @@ import com.example.bills.model.Bill;
 import com.example.bills.service.BillService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -11,13 +12,10 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/bills")
+@RequiredArgsConstructor
 public class BillController {
 
     private final BillService billService;
-
-    public BillController(BillService billService) {
-        this.billService = billService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
--- src/main/java/com/example/bills/model/Bill.java
+++ src/main/java/com/example/bills/model/Bill.java
@@ -6,10 +6,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
 public class Bill {
 
     @Id
@@ -22,52 +26,4 @@ public class Bill {
     private BillType type;
     @Enumerated(EnumType.STRING)
     private CreditCardName creditCardName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public BillType getType() {
-        return type;
-    }
-
-    public void setType(BillType type) {
-        this.type = type;
-    }
-
-    public CreditCardName getCreditCardName() {
-        return creditCardName;
-    }
-
-    public void setCreditCardName(CreditCardName creditCardName) {
-        this.creditCardName = creditCardName;
-    }
 }
